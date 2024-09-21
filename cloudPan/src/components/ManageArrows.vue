6fcef3a3cc41834fb3cd0299653f2caaeb820dfb
deslre <2135232170@qq.com>
2024-09-21 21:27:27 +0800

    feta: 添加操作按钮

--- cloudPan/src/components/ManageArrows.vue
+++ cloudPan/src/components/ManageArrows.vue
@@ -1,9 +1,7 @@
 <template>
     <div>
-        <!-- 触发按钮 -->
         <el-button :type="type" :icon="icon" @click="expandButton">{{ text }}</el-button>
 
-        <!-- 弹出的展示栏 (Dialog) -->
         <el-dialog title="箭头数据管理" v-model="dialogVisible" width="50%" :modal="true" :center="true" :lock-scroll="false"
             append-to-body>
 
@@ -28,26 +26,30 @@
                         <span>{{ scope.row.exist ? '是' : '否' }}</span>
                     </template>
                 </el-table-column>
+                <el-table-column label="操作">
+                    <template #default="scope">
+                        <el-button type="primary" @click="editRow(scope.row)" :icon="Edit" size="small" />
+                        <el-button type="danger" @click="deleteRow(scope.row)" :icon="Delete" size="small" />
+                    </template>
+                </el-table-column>
             </el-table>
 
             <!-- 分页组件 -->
             <el-pagination background layout="total, prev, pager, next" :total="total" :page-size="pageSize"
                 :current-page="currentPage" @current-change="handlePageChange"
-                style="text-align: right; margin-top: 10px;">
-            </el-pagination>
+                style="text-align: right; margin-top: 10px;" />
 
-            <!-- Dialog 的 footer -->
             <template #footer>
                 <el-button @click="dialogVisible = false">关闭</el-button>
             </template>
         </el-dialog>
     </div>
 </template>
+
 <script setup>
 import { ref, getCurrentInstance, computed } from 'vue'
-import { Edit } from '@element-plus/icons-vue'
+import { Edit, Delete } from '@element-plus/icons-vue'
 
-// 接收父组件传入的参数
 const props = defineProps({
     type: {
         type: String,
@@ -76,21 +78,13 @@ const expandButton = async () => {
     dialogVisible.value = true
 }
 
-// 控制 Dialog 显示/隐藏
 const dialogVisible = ref(false)
-
-// 表格数据
 const tableData = ref([])
-
-// 分页参数
 const total = ref(0)
 const pageSize = ref(10)
 const currentPage = ref(1)
-
-// 搜索框输入的内容
 const searchQuery = ref('')
 
-// 获取当前页数据
 const getPageData = async () => {
     const result = await proxy.Request({
         url: api.getPageData,
@@ -104,7 +98,6 @@ const getPageData = async () => {
     total.value = result.data.totalCount
 }
 
-// 添加箭头数据
 const addArrowsData = async () => {
     const result = await proxy.Request({
         url: api.addArrowsData,
@@ -114,23 +107,21 @@ const addArrowsData = async () => {
     console.log('addArrowsData ------> ', result);
 }
 
-// 更新箭头数据
-const updateArrowsData = async () => {
-    const result = await proxy.Request({
-        url: api.updateArrowsData,
-        showLoading: true,
-        params: {}
-    })
-    console.log('updateArrowsData ------> ', result);
+const editRow = (row) => {
+    console.log('Edit row:', row)
+    // 这里可以弹出编辑表单对话框，编辑 row 数据
 }
 
-// 监听页码变化
+const deleteRow = (row) => {
+    console.log('Delete row:', row)
+    // 这里可以弹出确认对话框，进行删除操作
+}
+
 const handlePageChange = (page) => {
     currentPage.value = page
     getPageData()
 }
 
-// 计算当前页展示的数据
 const filteredTableData = computed(() => {
     if (searchQuery.value) {
         return tableData.value.filter(item =>
@@ -140,6 +131,7 @@ const filteredTableData = computed(() => {
     return tableData.value
 })
 </script>
+
 <style scoped>
 .search-container {
     display: flex;
